0a667cbf41be905712028b0495958c1e8a7156d4
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2020-02-01 16:39:36 +0200

    bot initial version

---
+++ bot.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+const axios = require('axios');
+
+const quoteOfTheDay = async () => {
+    let quote, author;
+    try {
+        const { data } = await axios.get('https://api.quotable.io/random');
+        quote = data.content;
+        author = data.author;
+        return  `“${quote}” by ${author}`;    
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+client.on('ready', async  () => {
+    // Welcome message for starting the bot
+    console.log(`logged in as ${client.user.tag}!`);
+     
+    if (new Date().getHours() === 10) {
+        const quote = await quoteOfTheDay();
+        const generalChannelId = client.channels.find(channel => channel.name === 'general').id;
+        client.channels.get(generalChannelId).send(quote);
+    }
+})
+
+client.on('message', async msg => {
+    if (msg.content === 'quote') {
+        const quote = await quoteOfTheDay();
+        msg.reply(quote);
+    }
+})
+
+client.login(auth.token);
