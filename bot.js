80f301c3db484500f732c696f13cff2c01202a0b
Yosef Alnajjar <yosefalnajjarofficial@gmail.com>
2023-12-24 18:16:31 +0000

    refactor: delete unnecessary call

--- bot.js
+++ bot.js
@@ -1,51 +1,51 @@
-const http = require('http');
-const Discord = require('discord.js');
+const http = require("http");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const axios = require('axios');
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 
-http.createServer((req, res) => {
+http
+  .createServer((req, res) => {
     res.writeHead(200, {
-        'Content-type': 'text/plain'
+      "Content-type": "text/plain",
     });
     res.write(`server started running at ${Date.now()}`);
     res.end();
-}).listen(port);
-
-setInterval(() => {
-    http.get('http://wise-man-bot.herokuapp.com/');
-}, 1200000);
+  })
+  .listen(port);
 
 const quoteOfTheDay = async () => {
-    let quote, author;
-    try {
-        const { data } = await axios.get('https://api.quotable.io/random');
-        quote = data.content;
-        author = data.author;
-        return `“${quote}” by ${author}`;
-    } catch (error) {
-        console.log(error);
-    }
-}
+  let quote, author;
+  try {
+    const { data } = await axios.get("https://api.quotable.io/random");
+    quote = data.content;
+    author = data.author;
+    return `“${quote}” by ${author}`;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-client.on('ready', () => {
-    // Welcome message for starting the bot
-    console.log(`logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  // Welcome message for starting the bot
+  console.log(`logged in as ${client.user.tag}!`);
 
-    setInterval(async () => {
-        const quote = await quoteOfTheDay();
-        const generalChannelId = client.channels.find(channel => channel.name === 'general').id;
-        client.channels.get(generalChannelId).send(quote);
-    }, 86400000);
-})
+  setInterval(async () => {
+    const quote = await quoteOfTheDay();
+    const generalChannelId = client.channels.find(
+      (channel) => channel.name === "general"
+    ).id;
+    client.channels.get(generalChannelId).send(quote);
+  }, 86400000);
+});
 
-client.on('message', async msg => {
-    if (msg.content === 'quote') {
-        const quote = await quoteOfTheDay();
-        msg.reply(quote);
-    }
-})
+client.on("message", async (msg) => {
+  if (msg.content === "/quote") {
+    const quote = await quoteOfTheDay();
+    msg.reply(quote);
+  }
+});
 
 client.login(process.env.TOKEN);
